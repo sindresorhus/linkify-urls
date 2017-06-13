9ce858fc01f0a7d54bd7390a148ace47723f1ef3
Sindre Sorhus <sindresorhus@gmail.com>
2017-06-13 02:52:06 +0200

    Stupid Node.js 4 bug with regex literal in arrow function

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 // - const urlRegex = require('url-regex');
 const escapeGoat = require('escape-goat');
 
-const urlRegex = () => /(http(s)?(:\/\/))(www\.)?[a-zA-Z0-9-_.]+(\.[a-zA-Z0-9]{2,})([-a-zA-Z0-9:%_+.~#?&//=]*)/g;
+const urlRegex = () => (/(http(s)?(:\/\/))(www\.)?[a-zA-Z0-9-_.]+(\.[a-zA-Z0-9]{2,})([-a-zA-Z0-9:%_+.~#?&//=]*)/g);
 
 module.exports = (input, options) => {
 	options = Object.assign({
