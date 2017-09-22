4db31ab79d62dc0adf6b3c086a6a68f955343918
Sindre Sorhus <sindresorhus@gmail.com>
2017-09-22 20:04:52 +0700

    Add failing test for ignoring git URLs
    Closes #6

--- test.js
+++ test.js
@@ -92,6 +92,11 @@ test('supports `@` in the URL path', t => {
 	t.is(m('https://sindresorhus.com/@foo'), '<a href="https://sindresorhus.com/@foo">https://sindresorhus.com/@foo</a>');
 });
 
+test.failing('skips Git URLs', t => {
+	const fixture = 'git+https://github.com/sindreorhus/ava';
+	t.is(m(fixture), fixture);
+});
+
 test('skips email addresses', t => {
 	t.is(m('sindre@example.com'), 'sindre@example.com');
 	t.is(m('www.sindre@example.com'), 'www.sindre@example.com');
