5021db1d9219b2fc73031fbbd6ef53cd8a0bf2c5
Pedro Pinto <pedropinto.web@gmail.com>
2018-11-30 14:18:03 +0000

    Fix skip prefixed URLs
    How: Adding a negative lookbehind on the main RegEx to ignore URLs preceded by
    Why: Negative lookbehinds seem to be supported since Node v6 with the  flag (ref: https://node.green/#ES2018-features--RegExp-Lookbehind-Assertions) and should also be supported in ES2019 (ref: https://tc39.github.io/ecma262/).

--- test.js
+++ test.js
@@ -119,7 +119,7 @@ test('supports `value` option as function', t => {
 	}), 'See <a href="https://github.com/sindresorhus.com/linkify-urls">github.com</a> for a solution');
 });
 
-test.failing('skips Git URLs', t => {
+test('skips Git URLs', t => {
 	const fixture = 'git+https://github.com/sindresorhus/ava';
 	t.is(m(fixture), fixture);
 });
